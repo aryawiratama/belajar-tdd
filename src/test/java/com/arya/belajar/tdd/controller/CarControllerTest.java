a2c4a03a3f9e95d2629469d5e2cc39e9bd9dc188
aryawiratama <stop.drunk@gmail.com>
2017-12-25 13:47:02 +0800

    test car controller class using mock mvc

--- src/test/java/com/arya/belajar/tdd/controller/CarControllerTest.java
+++ src/test/java/com/arya/belajar/tdd/controller/CarControllerTest.java
@@ -2,15 +2,26 @@ package com.arya.belajar.tdd.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CarController.class)
 public class CarControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     public void getCar_ShouldReturnCar()throws Exception{
-
+        mockMvc.perform(MockMvcRequestBuilders.get("/cars/jazz"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("Jazz"))
+                .andExpect(jsonPath("type").value("City Car"));
     }
 }
