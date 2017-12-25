31cbe716c98aac47ee921f48336732d17050bd99
aryawiratama <stop.drunk@gmail.com>
2017-12-25 15:11:18 +0800

    add controller end point for cars

--- src/main/java/com/arya/belajar/tdd/controller/CarController.java
+++ src/main/java/com/arya/belajar/tdd/controller/CarController.java
@@ -1,4 +1,17 @@
 package com.arya.belajar.tdd.controller;
 
+import com.arya.belajar.tdd.domain.Car;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cars")
 public class CarController {
+
+    @GetMapping("/{name}")
+    public Car getCar(@PathVariable String name){
+        return null;
+    }
 }
