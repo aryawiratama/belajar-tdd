4cf8da0c98a67da2d39534cc898dc03469009124
aryawiratama <stop.drunk@gmail.com>
2017-12-25 13:30:49 +0800

    add assertion to integration test

--- src/main/java/com/arya/belajar/tdd/domain/Car.java
+++ src/main/java/com/arya/belajar/tdd/domain/Car.java
@@ -1,4 +1,12 @@
 package com.arya.belajar.tdd.domain;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Car {
+    private String type;
+    private String name;
 }
--- src/test/java/com/arya/belajar/tdd/integration/CarIntegrationTest.java
+++ src/test/java/com/arya/belajar/tdd/integration/CarIntegrationTest.java
@@ -1,13 +1,19 @@
 package com.arya.belajar.tdd.integration;
 
 import com.arya.belajar.tdd.domain.Car;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,5 +25,8 @@ public class CarIntegrationTest {
     @Test
     public void testGetCar_ShouldReturnCar()throws Exception{
         ResponseEntity<Car> response = restTemplate.getForEntity("/cars/jazz", Car.class);
-    }
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response.getBody().getName(), equalToIgnoringCase("Jazz"));
+        assertThat(response.getBody().getType(), equalToIgnoringCase("City Car"));
 }
